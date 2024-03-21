da7c9993d5b9da9cf3c0e925f369f4a5e8c8d135
hanri777 <fjhanri@gmail.com>
2024-03-21 16:18:07 +0100

    Commit 21.03.24

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # BMMF_PBMC_breast_Project
 BMMF_PBMC_breast_Projec
+
+I downloaded the following .fastq files from the internet:
+SRR14530610.fastq
+SRR14530611.fastq
+SRR14530612.fastq
+SRR14530613.fastq
+SRR14530614.fastq
+SRR14530615.fastq
+SRR14530616.fastq
+SRR14530617.fastq
+
+1) Quality check of.fastq files with fastqc in terminal
+
+Terminal command:
+fastqc *.fastq
+
+2) Trim low quality reads with Trimmomatic-0.39 tool.
+
+Terminal Command:
+java -jar trimmomatic-0.39.jar SE -threads 4 inputFile.fastq outputFile_trimmed.fastq TRAILING:10 -phred33
+
+3) Quality check of trimmed data.
+
+Terminal Command:
+fastqc outputFile_trimmed.fastq
+
+4) Alignment of Reads with Reference Genome (grch38) using 
+	HISAT2 tool. 
+	
+Terminal Command:
+hisat2 -q --rna-strandness R -x  grch38/genome -U 	inputFile_trimmed.fastq | samtools sort -o 	outputFile_trimmed.bam
+
+5) Use .bam file and Gene Transfer Format (GTF) file to build the 	Feature Count Matrix  (Subread)
+
+Terminal Command:
+featureCounts -S 1 -a Homo_sapiens.GRCh38.106.gtf -o 	outputFile_featurecounts.txt inputFile_trimmed.bam
+
+I have 8 Count Matrix .txt files as output.
+And combined them to one .csv file.
+
+
+
--- Rcode.R
+++ Rcode.R
@@ -1,2 +1,123 @@
-# priv
-print("Hello")
+# Loading packages
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install("synapter", type = "binary")
+BiocManager::install("org.Hs.eg.db")
+BiocManager::install("AnnotationDbi")
+
+library(AnnotationDbi)
+library(org.Hs.eg.db)
+
+library(readxl)
+library(dplyr)
+library(DESeq2)
+library(vsn)
+library(readxl)
+library(ggplot2)
+library(tidyverse)
+library(grid)
+library(ComplexHeatmap)
+library(ggrepel)
+library(EnhancedVolcano)
+library(pheatmap)
+library(Rtsne)
+
+# Loading Count Matrix (data frame)
+count_matrix_orig <- read.csv("BMMF_PBMC_breast_CountMatrix.csv")
+class(count_matrix_orig)
+dim(count_matrix_orig)
+str(count_matrix_orig)
+
+count_mat_df <- count_matrix_orig
+
+# Extract sample names from the first row of each column and set them as column names
+colnames(count_mat_df) <- sapply(count_mat_df[1, ], 
+                                      function(x) sub("\\./(.*)_.*", "\\1", x))
+
+# Remove the first row after assigning column names
+count_mat_df <- count_mat_df[-1, ]
+
+# Set Geneid column as row names
+rownames(count_mat_df) <- count_mat_df$Geneid
+
+# Delete the first column
+count_mat_df <- count_mat_df[, -1]
+
+# Convert character values to numerical values
+count_mat_df <- count_mat_df %>% mutate_at(1:length(count_mat_df),
+                                           as.numeric)
+
+str(count_mat_df)
+head(count_mat_df)
+dim(count_mat_df)
+
+
+# Loading meta data (originally converted .txt file to .xlsx file)
+meta_data <- read_xlsx('SraRunTable.xlsx')
+class(meta_data)
+metadata_df <- as.data.frame(meta_data)
+class(metadata_df)
+str(metadata_df)
+
+# Set Samples column as row names
+rownames(metadata_df) <- metadata_df$Run
+dim(metadata_df)
+head(metadata_df)
+
+# Basic Quality Check of Feature Count Matrix and Meta Data
+# Check if row names in metadata_df matches col names in count data
+all(colnames(count_mat_df) %in% rownames(metadata_df))
+# Checking order of row names and column names
+all(rownames(metadata_df) == colnames(count_mat_df))
+
+
+# Building DESeq Data Set
+dds <- DESeqDataSetFromMatrix(countData = count_mat_df, 
+                              colData = metadata_df,
+                              design = ~ disease_state)
+dds
+
+# Removing Low Counts Reads Genes (leaky expression of genes)
+keep <- rowSums(counts(dds)) >= 10
+dds <- dds[keep,]
+dds
+
+
+#Differential Analysis of Gene (compare cancer with normal)
+# Set reference value for DEG analysis 
+dds$disease_state <- relevel(dds$disease_state, ref = "normal")
+
+# Perform Differential Analysis of Gene
+deg <- DESeq(dds)
+
+# Getting results and sorting as data frame
+DESeqRes <- results(deg, alpha = 0.05)
+DESeqRes <- DESeqRes[!is.na(DESeqRes$padj),]
+DESeqRes <- DESeqRes[order(DESeqRes$padj),]
+
+summary(DESeqRes)
+DESeqRes_df <- as.data.frame(DESeqRes)
+class(DESeqRes_df)
+
+# Converting Gene ID to Gene Name
+# Create new column Symbol for Gene Name in data frame
+DESeqRes_df_Genenames <- DESeqRes_df
+DESeqRes_df_Genenames$Symbol <- mapIds(org.Hs.eg.db, rownames(DESeqRes_df_Genenames),
+                                  keytype = "ENSEMBL",
+                                  column = "SYMBOL")
+
+str(DESeqRes_df_Genenames)
+
+#Getting idea about best Genes
+best_genes_names <- DESeqRes_df_Genenames %>%
+  arrange(padj) %>%
+  head(20)
+
+str(best_genes_names)
+
+# Writing to .csv file
+write.csv(DESeqRes_df_Genenames, "output_files/DESeqRes_df_Genenames.csv")
+write.csv(best_genes_names, "output_files/best_genes_names.csv")
+
+
+
